0117a03cfd160bda51ac532bf8133c52e3073800
Dima <shishovdima777@gmail.com>
2023-12-16 11:04:21 +0300

    fixed TODO

--- src/main/java/com/timetobeat/timetobeat/controllers/GameController.java
+++ src/main/java/com/timetobeat/timetobeat/controllers/GameController.java
@@ -27,8 +27,8 @@ public class GameController {
 
     @GetMapping()
     public List<GameDTO> getAllGames() {
-        List<GameDTO> gameDTOList = gameService.findAll().stream().map(this::convertToGameDto).toList(); // TODO 1.
-        return gameDTOList;
+        return gameService.findAll().stream().map(this::convertToGameDto).toList();
+
     }
     @GetMapping("game/{id}")
     public GameDTO getGame(@PathVariable int id) {
