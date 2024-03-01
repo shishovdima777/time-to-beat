e017d2161a88d99f2e81694fe4eb20251c4f8924
Dima <shishovdima777@gmail.com>
2024-03-01 21:13:31 +0300

    added default authentication

--- src/main/java/com/timetobeat/timetobeat/config/SecurityConfig.java
+++ src/main/java/com/timetobeat/timetobeat/config/SecurityConfig.java
@@ -1,11 +1,36 @@
 package com.timetobeat.timetobeat.config;
 
+import com.timetobeat.timetobeat.services.serviceImpls.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
-//TODO
+public class SecurityConfig {
+    private final UserServiceImpl userService;
+    @Autowired
+    public SecurityConfig(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+     http.
+             authorizeHttpRequests((auth) ->
+                     auth.anyRequest().authenticated()
+             )
+             .httpBasic(Customizer.withDefaults());
+
+     return http.build();
+ }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
--- src/main/java/com/timetobeat/timetobeat/services/serviceImpls/UserServiceImpl.java
+++ src/main/java/com/timetobeat/timetobeat/services/serviceImpls/UserServiceImpl.java
@@ -37,7 +37,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = usersRepository.getUserByUsername(userDetails.getUsername());
+        User user = usersRepository.getUserByUsername(username);
 
         if(user == null) {
             throw new UsernameNotFoundException("User not found!");
